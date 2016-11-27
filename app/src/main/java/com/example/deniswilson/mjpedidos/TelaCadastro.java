887cd0f3485294a1afb38ddd28cb0c99c2756da8
DenisWP <denis.dabliu@gmail.com>
2016-11-27 17:43:25 -0200

    Instanciando componentes iniciais.

--- app/src/main/java/com/example/deniswilson/mjpedidos/TelaCadastro.java
+++ app/src/main/java/com/example/deniswilson/mjpedidos/TelaCadastro.java
@@ -2,12 +2,36 @@ package com.example.deniswilson.mjpedidos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TelaCadastro extends AppCompatActivity {
+
+    EditText nome, telefone, endereço, numero, bairro, cidade, cep, email;
+    Button salvar, cancelar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_cadastro);
+
+
+        nome = (EditText) findViewById(R.id.edtNome);
+        telefone = (EditText) findViewById(R.id.edtTel);
+        endereço = (EditText) findViewById(R.id.edtLongradouro);
+        numero = (EditText) findViewById(R.id.edtNumero);
+        bairro = (EditText) findViewById(R.id.edtBairro);
+        cidade = (EditText) findViewById(R.id.edtCidade);
+        cep = (EditText) findViewById(R.id.edtCep);
+        email = (EditText) findViewById(R.id.edtEmail);
+
+        salvar = (Button) findViewById(R.id.btnSalvar);
+        cancelar = (Button) findViewById(R.id.btnCancelar);
+
+
+
+
     }
 }
