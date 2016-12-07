51d6db37c6ef0cc98404650ebbf7e8db8317c87d
DenisWP <denis.dabliu@gmail.com>
2016-12-07 00:12:12 -0200

    Listar Produtos [FUNCIONANDO ! PHP - JSON - MYSQL]

--- app/src/main/java/com/example/deniswilson/mjpedidos/ListarProdutos.java
+++ app/src/main/java/com/example/deniswilson/mjpedidos/ListarProdutos.java
@@ -1,12 +1,26 @@
 package com.example.deniswilson.mjpedidos;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ListarProdutos extends AppCompatActivity {
 
     ListView listarProdutos;
+    String url = "http://192.168.0.103/mjpedidos/listar.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,95 @@ public class ListarProdutos extends AppCompatActivity {
         setContentView(R.layout.listar_produtos);
 
         listarProdutos = (ListView) findViewById(R.id.lstProdutos);
+        Listar l = new Listar(this,url,listarProdutos);
+        l.execute();
+
     }
+
+
+
+
+
+    public class Listar extends AsyncTask<Void, Integer, String> {
+
+        Context context;
+        String add;
+        ListView list;
+
+        ProgressDialog pd;
+
+        public Listar(Context context, String add, ListView list){
+            this.context = context;
+            this.add = add;
+            this.list = list;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pd = new ProgressDialog(context);
+            pd.setTitle("Baixando ... ");
+            pd.setMessage("Atualizando, por favor aguarde ...");
+            pd.show();
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            String data=Baixar();
+            return data;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            pd.dismiss();
+            if(s != null)
+            {
+                Parser p=new Parser(context,s,list);
+                p.execute();
+            }else
+            {
+                Toast.makeText(context,"Unable to download data",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+        private String Baixar()
+        {
+            InputStream is=null;
+            String line =null;
+            try {
+                URL url=new URL(add);
+                HttpURLConnection con= (HttpURLConnection) url.openConnection();
+                is=new BufferedInputStream(con.getInputStream());
+                BufferedReader br=new BufferedReader(new InputStreamReader(is));
+                StringBuffer sb=new StringBuffer();
+                if(br != null) {
+                    while ((line=br.readLine()) != null) {
+                        sb.append(line+"n");
+                    }
+                }else {
+                    return null;
+                }
+                return sb.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                if(is != null)
+                {
+                    try {
+                        is.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            return null;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/deniswilson/mjpedidos/Parser.java
@@ -0,0 +1,103 @@
+package com.example.deniswilson.mjpedidos;
+
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Denis Wilson on 06/12/2016.
+ */
+
+public class Parser extends AsyncTask <Void, Integer, Integer> {
+
+
+    Context context;
+    ListView list;
+    String data;
+    ArrayList<String> players=new ArrayList<>();
+    ProgressDialog pd;
+
+
+    public Parser(Context context, String data, ListView list) {
+        this.context = context;
+        this.data = data;
+        this.list = list;
+    }
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd=new ProgressDialog(context);
+        pd.setTitle("Baixando");
+        pd.setMessage("Baixando ... por favor aguarde.");
+        pd.show();
+    }
+    @Override
+    protected Integer doInBackground(Void... params) {
+        return this.parse();
+    }
+    @Override
+    protected void onPostExecute(Integer integer) {
+        super.onPostExecute(integer);
+        if(integer == 1)
+        {
+            //ADAPTER
+            ArrayAdapter<String> adapter=new ArrayAdapter<String>(context,android.R.layout.simple_list_item_1,players);
+            //ADAPT TO LISTVIEW
+            list.setAdapter(adapter);
+            //LISTENET
+            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                   // Snackbar.make(view,players.get(position),Snackbar.LENGTH_SHORT).show();
+
+                }
+            });
+        }else
+        {
+            Toast.makeText(context,"Unable to Parse",Toast.LENGTH_SHORT).show();
+        }
+        pd.dismiss();
+    }
+
+
+
+    //PARSE RECEIVED DATA
+    private int parse()
+    {
+        try
+        {
+            //ADD THAT DATA TO JSON ARRAY FIRST
+            JSONArray ja=new JSONArray(data);
+            //CREATE JO OBJ TO HOLD A SINGLE ITEM
+            JSONObject jo=null;
+            players.clear();
+            //LOOP THRU ARRAY
+            for(int i=0;i<ja.length();i++)
+            {
+                jo=ja.getJSONObject(i);
+                //RETRIOEVE NAME
+                String name=jo.getString("nome_produto");
+                String valor=jo.getString("valor");
+                //ADD IT TO OUR ARRAYLIST
+                players.add(name +"\n"+ valor);
+            }
+            return 1;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
+
